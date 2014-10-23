a0195a2d470097f96e1108e1a5321e45507a999c
Michael Buckbee <michaelbuckbee@gmail.com>
2014-10-23 15:43:33 -0400

    Adding swither.ps1 and send-email.ps1 examples

---
+++ scripts/send-email.ps1
@@ -0,0 +1,22 @@
+﻿# This script schould be integrated into the switcher.ps1 script called from DatAlert
+# which handles the logic of which script is associated with which rule.
+# 
+# The variables from DatAlert will not be available in this script otherwise.
+# 
+# This script relies upon the Send-MailMessage CmdLet
+# 
+# Official MSDN Documentation at: http://msdn.microsoft.com/en-US/us-en/windows/hh849925
+#
+# Email Customization Variables
+# 
+# Receipient (sysadmin to receive the alert)
+$recipientEmail = 'sysadmin@example.com'
+
+# From Email - will need to be an account with access to send email
+$fromEmail = 'alerts@example.com'
+
+# SMTP Server
+$smtpServer = 'mail.example.com'
+
+
+send-mailmessage -to $recipientEmail -from $fromEmail -SmtpServer $smtpServer -subject "Varonis Alert: $($ruleName) of user $($affectedObject) on $($ipAddressHost)/$($path)"
---
+++ switcher.ps1
@@ -0,0 +1,26 @@
+﻿Import-Module Varonis -Force -ArgumentList $args
+
+# Rule IDs are listed in the DatAlert Real-Time Alerts screen
+# Each Rule ID corresponds to a different command below. 
+#
+# Inclusion of the Varonis Powershell module passes in the $ruleID variable to this script (when configured)
+# Please see: https://github.com/varonis/powershell for directions
+
+# Extend this to match each RuleID that you are passing a Command-line script Alert Method to
+
+
+switch ($ruleID){
+
+    1 {
+        "Rule ID 1 was called"
+    }
+
+    2 {
+        "Rule ID 2 was called"
+    }
+
+    default {
+        "Rule ID Not Found"
+    }
+    
+}
