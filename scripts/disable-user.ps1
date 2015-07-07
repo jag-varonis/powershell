8e8381206a03c07decea893de7abc6fa370b8415
Michael Buckbee <michaelbuckbee@gmail.com>
2015-07-07 14:44:40 -0400

    Updating to reflect DL output

--- scripts/disable-user.ps1
+++ scripts/disable-user.ps1
@@ -1,3 +1,5 @@
-﻿Import-Module Varonis -Force -ArgumentList $args
-
-Disable-ADAccount -Identity $actingObject
+Import-Module Varonis -Force -ArgumentList $args
+ 
+$actingObject = $args[0].trimstart("domain.com\")
+$user = get-aduser -identity $actingObject | get-aduser -properties * | select samAccountName
+Disable-ADAccount -Identity $user
